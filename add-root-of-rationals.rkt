4f0d08d0623c966b73e2710323d7b4b78f05f684
Jos Koot <jos.koot@gmail.com>
2019-06-22 15:52:12 +0200

    Add files via upload

--- add-root-of-rationals.rkt
+++ add-root-of-rationals.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (require (only-in math/number-theory factorize))
-(provide add-root-of-rationals)
+(provide add-roots-of-rationals)
 
 ; An rr is plus or minus the square root of a nonnegative rational number.
 ; An rr can be represented exactly by an rrr.
@@ -11,37 +11,40 @@
 ; An error is raised when the square of the sum is not a rational number.
 ; See examples at the end of this module.
 
-(define (add-root-of-rationals . rrrs)
- (cond
-  ((null? rrrs) 0)
-  ((null? (cdr rrrs)) (car rrrs))
- (else
-  (let*
-   ((rrrs (remove-opponents rrrs))
-    (signs (map (λ (x) (if (positive? x) +1 -1)) rrrs))
-    (rrrs (map abs rrrs))
-    (numerators (map numerator rrrs))
-    (denominators (map denominator rrrs))
-    (gcd-numerators (apply gcd numerators))
-    (gcd-denominators (apply gcd denominators))
-    (numerators (map (λ (x) (/ x gcd-numerators)) numerators))
-    (denominators (map (λ (x) (/ x gcd-denominators)) denominators))
-    (numerators (map sqrt-factorize numerators))
-    (denominators (map sqrt-factorize denominators))
-    (terms (map / numerators denominators))
-    (sum (apply + (map * signs terms)))
-    (sum (* sum (abs sum))))
-   (* (/ gcd-numerators gcd-denominators) sum)))))
+;----------------------------------------------------------------------------------------------------
 
-(define (remove-opponents rrrs)
- (let loop ((r '()) (rrrs rrrs))
+(define (add-roots-of-rationals . rrrs)
+ (let ((rrrs (read-hash (combine-equals rrrs))))
   (cond
-   ((null? rrrs) r)
+   ((null? rrrs) 0)
+   ((null? (cdr rrrs)) (car rrrs))
    (else
-    (define rrr (car rrrs))
-    (define -rrr (- rrr))
-    (define rop (remove-opponent -rrr (cdr rrrs)))
-    (if rop (loop r rop) (loop (cons rrr r) (cdr rrrs)))))))
+    (let*
+     ((signs (map (λ (x) (if (positive? x) +1 -1)) rrrs))
+      (rrrs (map abs rrrs))
+      (numerators (map numerator rrrs))
+      (denominators (map denominator rrrs))
+      (gcd-numerators (apply gcd numerators))
+      (gcd-denominators (apply gcd denominators))
+      (numerators (map (λ (x) (/ x gcd-numerators)) numerators))
+      (denominators (map (λ (x) (/ x gcd-denominators)) denominators))
+      (numerators (map sqrt-factorize numerators))
+      (denominators (map sqrt-factorize denominators))
+      (terms (map / numerators denominators))
+      (sum (apply + (map * signs terms)))
+      (sum (* sum (abs sum))))
+     (* (/ gcd-numerators gcd-denominators) sum))))))
+
+(define (combine-equals rrrs)
+ (for/fold ((h (hash))) ((rrr rrrs))
+  (define abs-rrr (abs rrr))
+  (if (zero? rrr) h
+   (hash-set h abs-rrr ((if (negative? rrr) sub1 add1) (hash-ref h abs-rrr 0))))))
+
+(define (read-hash h)
+ (for/fold ((rrrs '())) (((rrr n) (in-hash h)))
+  (if (zero? n) rrrs
+   (cons (* n (abs n) rrr) rrrs))))
 
 (define (remove-opponent -x rrrs)
  (let/ec ec
@@ -58,16 +61,25 @@
 (define (root z)
  (define p (car z))
  (define q (cadr z))
- (unless (even? q)
-  (error 'root "square expected, found: ~s^~s" p q))
+ (unless (even? q) (error 'root "square expected, found: ~s^~s" p q))
  (expt p (/ q 2)))
 
-#|
-(add-root-of-rationals 3 -12) ; --> -3 because √3-√12 = √3(1-√4) = -√3
-(add-root-of-rationals 1/3 -1/108 5 -5 -1/5 1/5 -5 5) ; --> 25/108 because:
-; √(1/3)-√(1/108) = √(1/3)(1-√(1/36)) = √(1/3)(1-1/6) = √(1/3)(5/6) = √(1/3)√(25/36) = √(25/108)
-(add-root-of-rationals -1/3 1/108 5 -5 -1/5 1/5 -5 5) ; --> -25/108
-(add-root-of-rationals 1/2 1/2) ; --> 2 because √(1/2)+√(1/2) = 2√(1/2) = √(4/2) = √2
-(add-root-of-rationals 1/3 1/3) ; --> 4/3 because √(1/3)+√(1/3) = 2√(1/3) = √(4/3)
-(add-root-of-rationals 1/2 1/3) ; --> error: add-root-of-rationals: square expected, found: 3
-|#
+#;(begin
+   
+ (let ([cp (current-print)]) ; with thanks to Roby Findler
+  (current-print
+   (λ (x)
+     (define sp (open-output-string))
+     (parameterize ([current-output-port sp])
+       (cp x))
+     (display (get-output-string sp)))))
+ (add-roots-of-rationals 3 -12) ; --> -3 because √3-√12 = √3(1-√4) = -√3
+ (add-roots-of-rationals 1/3 -1/108 5 -5 -1/5 1/5 -5 5) ; --> 25/108 because:
+ ; √(1/3)-√(1/108) = √(1/3)(1-√(1/36)) = √(1/3)(1-1/6) = √(1/3)(5/6) = √(1/3)√(25/36) =
+ ; √(25/108) 
+ (add-roots-of-rationals -1/3 1/108 5 -5 -1/5 1/5 -5 5) ; --> -25/108
+ (add-roots-of-rationals 1/2 1/2) ; --> 2 because √(1/2)+√(1/2) = 2√(1/2) = √(4/2) = √2
+ (add-roots-of-rationals 1/3 1/3) ; --> 4/3 because √(1/3)+√(1/3) = 2√(1/3) = √(4/3)
+ (add-roots-of-rationals 1/2 1/3)) ; --> error: add-root-of-rationals:
+;                                        square expected, found: 3^1
+
